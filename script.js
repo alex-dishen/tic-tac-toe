7c4649049b8bf399f31b0d0803e93e0c7333e296
Alex <didyshen.oleksandr@gmail.com>
2022-10-19 21:01:03 +0300

    Refactor the code

--- script.js
+++ script.js
@@ -1,134 +1,169 @@
-const modeMenu = document.querySelector('.menu');
-const modes = document.querySelectorAll('.mode');
-const playModePreview = document.querySelector('.play-mode');
-const fightButton = document.querySelector('.fight-button');
-const backBtns = document.querySelectorAll('.back-navigation');
-const playground = document.querySelector('.playground');
+const Player = (name) => {};
 
-modes.forEach(mode => {
-    mode.addEventListener('click', () => {
+const gameBoard = (() => {
+    const board = ['', '', '', '', '', '', '', '', ''];
+})();
+
+const displayController = (() => {
+    // MODE MENU
+    const modeMenu = document.querySelector('.menu');
+    const modes = document.querySelectorAll('.mode');
+    
+    // PRE-MATCH
+    const playModePreview = document.querySelector('.play-mode');
+    const firstPlayerInput = document.querySelector('.player-1-input');
+    const secondPlayerInput = document.querySelector('.player-2-input');
+    const aiLevels = document.querySelector('.ai-levels');
+    const fightButton = document.querySelector('.fight-button');
+    const backBtns = document.querySelectorAll('.back-navigation');
+    const easyLevel = document.querySelector('.easy');
+    const midLevel = document.querySelector('.mid');
+    const hardLevel = document.querySelector('.hard');
+    
+    // PLAYERS ATTRIBUTES
+    const opponentFaces = document.querySelectorAll('.opponent-face');
+    const opponentSigns = document.querySelectorAll('.opponent-sign');
+    const opponentWins = document.querySelector('.opponent-wins');
+    const smallOpponentWins = document.querySelector('.small-opponent-wins');
+    const firstPlayerName = document.querySelector('.first-player-name');
+    const secondPlayerName = document.querySelector('.second-player-name');
+    
+    // PLAYGROUND
+    const playground = document.querySelector('.playground');
+    const level = document.getElementById('level');
+
+    const showTwoPlayersPrematch = () => {
+        opponentFaces.forEach(opponentFace => {
+            opponentFace.setAttribute('src', 'img/player-blue.svg');
+        });
+        opponentSigns.forEach(opponentSign => {
+            opponentSign.setAttribute('src', 'img/circle-blue.svg');
+        });
+        aiLevels.style.display = 'none';
+        firstPlayerInput.style.display = 'block';
+        secondPlayerInput.style.display = 'block';
+    };
+
+    const showAiPrematch = () => {
+        opponentFaces.forEach(opponentFace => {
+            opponentFace.setAttribute('src', 'img/robot.svg');
+        });
+        opponentSigns.forEach(opponentSign => {
+            opponentSign.setAttribute('src', 'img/circle-red.svg');
+        });
+        firstPlayerInput.style.display = 'none';
+        secondPlayerInput.style.display = 'none';
+        aiLevels.style.display = 'flex';
+    };
+    
+    const setDifficulty = () => {
+        easyLevel.addEventListener('click', () => {
+            easyLevel.classList.add('chosen-level');
+            midLevel.classList.remove('chosen-level');
+            hardLevel.classList.remove('chosen-level');
+            //if user changes levels a few times the classes will be stored in an element
+            //to prevent errors we clean element from classes every single time
+            level.removeAttribute('class');
+            level.classList.add('easy');
+            level.textContent = 'EASY'
+        });
+        midLevel.addEventListener('click', () => {
+            midLevel.classList.add('chosen-level');
+            easyLevel.classList.remove('chosen-level');
+            hardLevel.classList.remove('chosen-level');
+            level.removeAttribute('class');
+            level.classList.add('mid');
+            level.textContent = 'MID'
+        });
+        hardLevel.addEventListener('click', () => {
+            hardLevel.classList.add('chosen-level');
+            midLevel.classList.remove('chosen-level');
+            easyLevel.classList.remove('chosen-level');
+            level.removeAttribute('class');
+            level.classList.add('hard');
+            level.textContent = 'HARD'
+        });
+    };
+
+    const setPlaygroundForPlayers = () => {
+        opponentWins.style.color = 'var(--blue)';
+        smallOpponentWins.style.color = 'var(--blue)';
+        secondPlayerName.style.color = 'var(--blue)';
+        level.style.display = 'none';
+        secondPlayerName.style.display = 'block';
+        firstPlayerName.textContent = firstPlayerInput.value || 'Player 1';
+        secondPlayerName.textContent = secondPlayerInput.value || 'Player 2';
+    };
+
+    const setPlaygroundForAi = () => {
+        opponentWins.style.color = 'var(--red)';
+        smallOpponentWins.style.color = 'var(--red)';
+        firstPlayerName.textContent = 'Human';
+        secondPlayerName.style.display = 'none';
+        level.style.display = 'block';
+        if(!level.getAttribute('class')) {
+            level.classList.add('mid');
+            level.textContent = 'MID';
+        }
+    };
+
+    const clearPrematch = (() => {
+        const showModeMenu = () => {
+            playground.style.display = 'none';
+            playModePreview.style.display = 'none';
+            modeMenu.style.display = 'flex';
+        };
+
+        const twoPlayers = () => {
+            firstPlayerInput.value = '';
+            secondPlayerInput.value = '';
+        };
+
+        const ai = () => {
+            easyLevel.classList.remove('chosen-level');
+            midLevel.classList.remove('chosen-level');
+            hardLevel.classList.remove('chosen-level');
+        };
+
+        return { showModeMenu ,twoPlayers, ai }
+    })();
+
+    const play = (mode) => {
         const modeName = mode.getAttribute('data-name');
-        const opponentFaces = document.querySelectorAll('.opponent-face');
-        const opponentSigns = document.querySelectorAll('.opponent-sign');
-        const firstPlayerInput = document.querySelector('.player-1-input');
-        const secondPlayerInput = document.querySelector('.player-2-input');
-        const aiLevels = document.querySelector('.ai-levels');
-        const opponentWins = document.querySelector('.opponent-wins');
-        const smallOpponentWins = document.querySelector('.small-opponent-wins');
-        const firstPlayerName = document.querySelector('.first-player-name');
-        const secondPlayerName = document.querySelector('.second-player-name');
-        const level = document.getElementById('level');
-
         modeMenu.style.display = 'none';
         playModePreview.style.display = 'flex';
 
         if(modeName === 'twoPlayers') {
-            opponentFaces.forEach(opponentFace => {
-                opponentFace.setAttribute('src', 'img/player-blue.svg');
-            });
-            opponentSigns.forEach(opponentSign => {
-                opponentSign.setAttribute('src', 'img/circle-blue.svg');
-            });
-            aiLevels.style.display = 'none';
-            firstPlayerInput.style.display = 'block';
-            secondPlayerInput.style.display = 'block';
-            opponentWins.style.color = 'var(--blue)';
-            smallOpponentWins.style.color = 'var(--blue)';
-            secondPlayerName.style.display = 'block';
+            showTwoPlayersPrematch();
+            fightButton.addEventListener('click', setPlaygroundForPlayers);
 
-            fightButton.addEventListener('click', () => {        
-                if(firstPlayerInput.value === '') {
-                    firstPlayerName.textContent = 'Player 1';
-                } else {
-                    firstPlayerName.textContent = firstPlayerInput.value;
-                }
-
-                if(secondPlayerInput.value === '') {
-                    secondPlayerName.textContent = 'Player 2'
-                } else {
-                    secondPlayerName.textContent = secondPlayerInput.value;
-                }
-                secondPlayerName.style.color = 'var(--blue)';
-            });
-            level.style.display = 'none';
             backBtns.forEach(btn => {
-                btn.addEventListener('click', () => {
-                    firstPlayerInput.value = '';
-                    secondPlayerInput.value = '';
-                });
+                btn.addEventListener('click', () => {clearPrematch.twoPlayers()});
             });
         }
         if(modeName === 'ai') {
-            const easyLevel = document.querySelector('.easy');
-            const midLevel = document.querySelector('.mid');
-            const hardLevel = document.querySelector('.hard');
-            opponentFaces.forEach(opponentFace => {
-                opponentFace.setAttribute('src', 'img/robot.svg');
-            });
-            opponentSigns.forEach(opponentSign => {
-                opponentSign.setAttribute('src', 'img/circle-red.svg');
-            });
-            aiLevels.style.display = 'flex';
-            firstPlayerInput.style.display = 'none';
-            secondPlayerInput.style.display = 'none';
-            firstPlayerName.textContent = 'Human';
-            opponentWins.style.color = 'var(--red)';
-            smallOpponentWins.style.color = 'var(--red)';
-            secondPlayerName.style.display = 'none';
-            level.style.display = 'block';
-            easyLevel.addEventListener('click', () => {
-                easyLevel.classList.add('chosen-level');
-                midLevel.classList.remove('chosen-level');
-                hardLevel.classList.remove('chosen-level');
-                //if user changes levels a few times the classes will be stored in an element
-                //to prevent errors we clean element from classes every single time
-                level.removeAttribute('class');
-                level.classList.add('easy');
-                level.textContent = 'EASY'
-            });
-            midLevel.addEventListener('click', () => {
-                midLevel.classList.add('chosen-level');
-                easyLevel.classList.remove('chosen-level');
-                hardLevel.classList.remove('chosen-level');
-                level.removeAttribute('class');
-                level.classList.add('mid');
-                level.textContent = 'MID'
-            });
-            hardLevel.addEventListener('click', () => {
-                hardLevel.classList.add('chosen-level');
-                midLevel.classList.remove('chosen-level');
-                easyLevel.classList.remove('chosen-level');
-                level.removeAttribute('class');
-                level.classList.add('hard');
-                level.textContent = 'HARD'
-            });
-            fightButton.addEventListener('click', () => {
-                firstPlayerName.textContent = 'Human';
-                if(level.getAttribute('class') === null) {
-                    level.classList.add('mid');
-                    level.textContent = 'MID';
-                }
-            });
+            showAiPrematch();
+            setDifficulty();
+            fightButton.addEventListener('click', setPlaygroundForAi);
+
             backBtns.forEach(btn => {
-                btn.addEventListener('click', () => {
-                easyLevel.classList.remove('chosen-level');
-                midLevel.classList.remove('chosen-level');
-                hardLevel.classList.remove('chosen-level');
-                });
+                btn.addEventListener('click', () => {clearPrematch.ai()});
             });
         }
-    })
-});
-
-fightButton.addEventListener('click', () => {
-    playModePreview.style.display = 'none';
-    playground.style.display = 'grid';
-});
-
-backBtns.forEach(btn => {
-    btn.addEventListener('click', () => {
-        playground.style.display = 'none';
-        playModePreview.style.display = 'none';
-        modeMenu.style.display = 'flex';
+    };
+    
+    modes.forEach(mode => {
+        mode.addEventListener('click', () => {play(mode)})
     });
-});
+
+    fightButton.addEventListener('click', () => {
+        playModePreview.style.display = 'none';
+        playground.style.display = 'grid';
+    });
+
+    backBtns.forEach(btn => {
+        btn.addEventListener('click', () => {clearPrematch.showModeMenu()});
+    });
+})();
+
+const gameController = (() => {})();
